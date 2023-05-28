6c0f7b9539bac2288203e15fdcd20affc6a17bf9
Mikołaj Sokołowski <114940112+SokolowskiMik@users.noreply.github.com>
2023-05-28 22:32:29 +0200

    templates MS
    templates MS

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'kebabop babob'
 
 from app import routes
 
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,54 @@
-from app import app
+from app import app, render_template, request, flash
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Mikolaj Sokolowski"
+    return render_template("home.html")
+
+@app.route('/extract', methods=['GET', 'POST'])
+def extract():
+    return render_template("extract.html")
+
+@app.route('/products', methods=['GET', 'POST'])
+def products():
+    return render_template("products.html")
+
+@app.route('/author')
+def author():
+    return render_template("author.html")
+
+@app.route('/product', methods=['GET', 'POST'])
+def product():
+    return render_template("product.html")
+
+@app.route('/charts', methods=['GET', 'POST'])
+def charts():
+    return render_template("charts.html")
+
+@app.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif len(password1) < 7:
+            flash('password must be at least 7 characters.')
+        elif password1 != password2:
+            flash("Passwords don't match.", category='error')
+        else:
+            flash('Account created!', category='success')
+
+    return render_template("sign_up.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
 
 @app.route('/name/', defaults={'name': "Anonim"})
 @app.route('/name/<name>')
